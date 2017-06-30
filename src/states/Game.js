a76f6f9fead8dbc58c3cb270180268fad051ea70
Fredric Dorothy <fredric.dorothy@vipaar.com>
2017-06-29 20:54:34 -0500

    Added example of perlin noise generation for ambient sounds

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,74 +1,64 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Player from '../sprites/Player'
-
 import config from '../config'
 
 export default class extends Phaser.State {
   init () {
     this.stage.backgroundColor = '#000'
   }
-  preload () {}
+  preload () {
+    game.load.audio('cricket', 'assets/cricket.mp3');
+    game.load.audio('wave1', 'assets/wave1.ogg');
+  }
 
   create () {
-    this.player = new Player({
-      game: this.game,
-      x: config.initial.start.x,
-      y: config.initial.start.y,
-      asset: 'ms'
-    })
+    this.seed1 = Math.random();
+    this.seed2 = Math.random();
+    this.seed3 = Math.random();
+    this.t = 0.0;
+    this.cricket = game.add.audio('cricket');
+    this.cricket.loopFull();
+    this.wave1 = game.add.audio('wave1');
+    this.wave1.loopFull();
+  }
 
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.map = game.add.tilemap('level');
-    this.map.addTilesetImage('tiles', 'gametiles');
+  graphNoise(seed, color) {
+    noise.seed(seed);
+    var ctx = game.context;
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    var pts = [];
+    for (var i=0; i<100; i++) {
+      var v = this.sample(this.t + i / 10.0);
+      pts.push(v);
+    }
+    ctx.moveTo(0, pts[0] * this.game.height / 2.0 + this.game.height / 2.0);
+    for (var i=1; i<100; i++) {
+      ctx.lineTo(i * 32.0 / 10.0, pts[i] * this.game.height / 2.0 + this.game.height / 2.0);
+    }
+    ctx.stroke();
+  }
 
-    this.doors = this.map.createLayer('doors');
-    this.bg = this.map.createLayer('background');
-    this.bounds = this.map.createLayer('bounds');
-
-    this.game.add.existing(this.player)
-
-    this.water = this.map.createLayer('water');
-    this.bg = this.map.createLayer('foreground');
-
-    this.bounds.resizeWorld();
-    this.map.setCollisionBetween(1, 2000, true, 'bounds');
-
-    this.water.alpha = 0.6;
-
-    this.cursor = this.game.input.keyboard.createCursorKeys();
-    this.game.input.keyboard.addKeyCapture([
-    	Phaser.Keyboard.LEFT,
-    	Phaser.Keyboard.RIGHT,
-    	Phaser.Keyboard.UP,
-    	Phaser.Keyboard.DOWN,
-    	Phaser.Keyboard.SPACEBAR
-    ]);
-
-    // shadowTexture = this.game.add.bitmapData(this.game.width, this.game.height);
-    // lightSprite = this.game.add.image(this.game.camera.x, this.game.camera.y, shadowTexture);
-    // lightSprite.blendMode = Phaser.blendModes.MULTIPLY;
+  sample(t) {
+    return noise.simplex2(t / 30.0, 0.0);
   }
 
   render () {
+    this.graphNoise(this.seed1, 'rgb(0,255,255)');
+    this.graphNoise(this.seed2, 'rgb(255,0,255)');
   }
 
   update() {
-    game.physics.arcade.collide(this.player, this.bounds);
-    var blocked = this.player.body.blocked.down;
-    if (this.cursor.left.isDown) {
-      this.player.moveLeft();
-    }
-    else if (this.cursor.right.isDown) {
-      this.player.moveRight();
-    }
-    else if (blocked) {
-      this.player.stop();
-    }
-    if (this.cursor.up.isDown) {
-      this.player.jump();
-    }
-    //lightSprite.reset(game.camera.x, game.camera.y);
-    //updateShadowTexture();
+		var dt = this.game.time.physicsElapsed;
+
+    noise.seed(this.seed1);
+    var v = this.sample(this.t);
+    this.cricket.volume = (v + 1.0) / 2.0;
+
+    noise.seed(this.seed2);
+    var v = this.sample(this.t);
+    this.wave1.volume = (v + 1.0) / 2.0;
+
+    this.t += dt;
   }
 }
